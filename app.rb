915ba67999c515735dc3d01b565851048f59bbb4
Chris MacNaughton <chmacnaughton@gmail.com>
2015-10-24 17:48:42 -0400

    handle badly formatted JSON better

--- app.rb
+++ app.rb
@@ -3,13 +3,19 @@ require 'sinatra'
 require 'rusty_json'
 require 'cgi'
 require 'erb'
-
+require 'pry'
 get '/' do
   erb :index, layout: :application
 end
 
 post '/' do
   json = params[:json]
+  begin
+    JSON.parse(json)
+  rescue
+    @alert = "You submitted badly formatted JSON, please try again!"
+    return erb :index, layout: :application
+  end
   @rusty = CGI::escapeHTML(RustyJson.parse(json))
   erb :rust, layout: :application
 end
